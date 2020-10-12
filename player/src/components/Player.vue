58834ea0bb08b24f9e601e89561c04ea80262f86
Emmanuel Durgoni <emmanuel@upian.com>
2020-10-12 15:07:32 +0200

    up

--- player/src/components/Player.vue
+++ player/src/components/Player.vue
@@ -3,7 +3,7 @@
     <video @timeupdate="onUpdate" class="w-full" src="/assets/video.mp4" controls=true muted></video>
 
     <div class="Player__layer absolute top-0 left-0 w-full h-full text-left pointer-events-none p-12 text-white">
-      <h1 class=" text-4xl font-bold">Mon texte</h1>
+      <h1 class="text-4xl font-bold" v-if="currentLayer">{{ currentLayer }}</h1>
     </div>
   </div>
 </template>
@@ -11,6 +11,11 @@
 <script>
 import data from '@/assets/data.json'
 export default {
+  data() {
+    return {
+      currentLayer: null
+    }
+  },
   mounted() {
     console.log(data)
     // const video = this.$el.querySelector('video')
@@ -20,8 +25,33 @@ export default {
     onUpdate(e) {
       const video = e.target
       const currentTime = video.currentTime
-      console.log(currentTime)
+
+      this.findCurrentLayer(currentTime)
+    },
+
+    findCurrentLayer(currentTime) {
+      // il faut parser data.layers (for of)
+      for (const layer of data.layers) {
+        const start = layer.start
+        const end = layer.start + layer.duration
+        layer.end = end
+        
+        if(currentTime >= start && currentTime <= end) {
+          this.currentLayer = layer
+        }
+      }
+
+      // Une fois qu'un currentLayer existe, on va vérifier si le currentTime est plus petit que son start OU plus grand que son end. Si oui, on met currentLayer à null
+      if (this.currentLayer) {
+        if (currentTime < this.currentLayer.start || currentTime > this.currentLayer.end) {
+          this.currentLayer = null
+        }
+      }
+      
+      // Il faut regarder si current time il est supérier ou égale layer.start et inférieur à layer.end (start + duration)
     }
+
+
   }
 }
 </script>
