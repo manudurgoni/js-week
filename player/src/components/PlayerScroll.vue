8e41d0fb5e51560482fc652bcd5be9846e5bf150
Emmanuel Durgoni <emmanuel@upian.com>
2020-10-12 16:09:35 +0200

    up

--- player/src/components/PlayerScroll.vue
+++ player/src/components/PlayerScroll.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="PlayerScroll">
-    <div class="PlayerScroll__video">
-      <video src="/assets/rocket.mp4"></video>
+    <div class="PlayerScroll__video fixed top-0 left-0 h-screen w-screen">
+      <video class="absolute object-cover w-full h-full" src="/assets/rocket.mp4"></video>
     </div>
   </div>
 </template>
