9b68a08e99eb3a3b8498fc3552c588b245f98fad
Emmanuel Durgoni <emmanuel@upian.com>
2020-10-12 15:18:39 +0200

    up

--- player/src/components/Player.vue
+++ player/src/components/Player.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="Player max-w-full mx-6 md:mx-12 lg:mx-auto lg:max-w-5xl relative">
-    <video @timeupdate="onUpdate" class="w-full" src="/assets/video.mp4" controls=true muted></video>
+  <div class="Player max-w-full mx-6 md:mx-12 lg:mx-auto lg:max-w-5xl">
+    <h1 class="text-5xl font-bold">Ma video</h1>
 
-    <div class="Player__layer absolute top-0 left-0 w-full h-full text-left pointer-events-none p-12 text-white">
-      <h1 class="text-4xl font-bold" v-if="currentLayer">{{ currentLayer }}</h1>
+    <div class="Player__inner relative">
+      <video @timeupdate="onUpdate" class="w-full" src="/assets/video.mp4" controls=true muted></video>
+
+      <div class="Player__layer absolute top-0 left-0 w-full h-full text-left pointer-events-none p-12 text-white">
+        <h1 class="text-4xl font-bold" v-if="currentLayer">{{ currentLayer }}</h1>
+      </div>
     </div>
   </div>
 </template>
@@ -36,12 +40,13 @@ export default {
         const end = layer.start + layer.duration
         layer.end = end
         
+        // On vérifie que le currentTime de la video est inclus entre la start et le end du layer, si oui on stock le layer dans la variable currentLayer
         if(currentTime >= start && currentTime <= end) {
           this.currentLayer = layer
         }
       }
 
-      // Une fois qu'un currentLayer existe, on va vérifier si le currentTime est plus petit que son start OU plus grand que son end. Si oui, on met currentLayer à null
+      // si currentLayer existe, on va vérifier si le currentTime est plus petit que son start OU plus grand que son end. Si oui, on met currentLayer à null.
       if (this.currentLayer) {
         if (currentTime < this.currentLayer.start || currentTime > this.currentLayer.end) {
           this.currentLayer = null
