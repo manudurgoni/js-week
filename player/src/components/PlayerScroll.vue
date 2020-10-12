112f7358deff4884912f27121684459f1c7b98d9
Emmanuel Durgoni <emmanuel@upian.com>
2020-10-12 16:14:03 +0200

    update

--- player/src/components/PlayerScroll.vue
+++ player/src/components/PlayerScroll.vue
@@ -3,5 +3,13 @@
     <div class="PlayerScroll__video fixed top-0 left-0 h-screen w-screen">
       <video class="absolute object-cover w-full h-full" src="/assets/rocket.mp4"></video>
     </div>
+    <div class="PlayerScroll__fake-height absolute"></div>
   </div>
 </template>
+
+<style lang="postcss" scoped>
+.PlayerScroll__fake-height {
+  height: 5000px;
+  width: 1px;
+}
+</style>
