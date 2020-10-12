7ea841b79b1293abd9f7bdd6e91215cc863ce91e
Emmanuel Durgoni <emmanuel@upian.com>
2020-10-12 10:03:49 +0200

    init

---
+++ player/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('glslify')
+      .test(/\.(glsl|vs|fs|vert|frag)$/)
+      .use('raw-loader')
+        .loader('raw-loader')
+        .end()
+      .use('glslify-loader')
+        .loader('glslify-loader')
+        .end()
+  }
+}
