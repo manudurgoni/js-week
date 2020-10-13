f308a3381d998793cf2bd7de30db4a84bd2cf864
Emmanuel Durgoni <emmanuel@upian.com>
2020-10-13 15:14:30 +0200

    update

--- integration/src/components/VHero.vue
+++ integration/src/components/VHero.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="Hero text-center">
-    <div class="Hero__container max-w-4xl mx-auto">
+    <div class="Hero__container max-w-4xl mx-auto pt-20">
       <h1 class="title text-blue-dark">Good design meets<br>great user experience</h1>
-      <p>For everyone, from beginners to experts</p>
+      <p class="my-6 text-blue-text text-lg">For everyone, from beginners to experts</p>
 
       <div class="buttons">
-        <a href="#" class="link-rounded">Buy Digits</a>
-        <a href="#" class="link-rounded link-rounded--white">See the Features</a>
+        <a href="#" class="link-rounded mx-3">Buy Digits</a>
+        <a href="#" class="link-rounded link-rounded--white mx-3">See the Features</a>
       </div>
 
-      <div class="illustration">
+      <div class="illustration mt-24">
         <img src="@/assets/illustration.svg" alt="Jeune femme qui tient un téléphone les cheveux au vent.">
       </div>
     </div>
