6d8a3053be3a0f00c83b194a03043c548d763677
Emmanuel Durgoni <emmanuel@upian.com>
2020-10-13 12:15:40 +0200

    up

--- integration/src/components/VHeader.vue
+++ integration/src/components/VHeader.vue
@@ -3,14 +3,14 @@
     <div class="MainHeader__container max-w-4xl mx-auto px-4 py-6 bg-red-100 flex">
       <img src="@/assets/logo.svg" alt="Digits logo">
 
-      <nav class="ml-20">
+      <nav class="ml-20 text-blue-dark">
         <router-link to="/">Accueil</router-link>
         <router-link to="/about">À propos</router-link>
       </nav>
 
       <div class="contact ml-auto ">
         <a href="#">+00 123 456 789</a>
-        <a href="#">Contact Us</a>
+        <a href="#" class="bg-blue-button">Contact Us</a>
       </div>
     </div>
   </header>
