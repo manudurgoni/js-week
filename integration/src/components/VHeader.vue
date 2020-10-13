2d29a1c60b3fe6282a3381a62db0b419f2cb33cd
Emmanuel Durgoni <emmanuel@upian.com>
2020-10-13 11:35:43 +0200

    up

--- integration/src/components/VHeader.vue
+++ integration/src/components/VHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="MainHeader">
-    <div class="MainHeader__container">
+    <div class="MainHeader__container max-w-4xl mx-auto">
       <img src="@/assets/logo.svg" alt="Digits logo">
     </div>
   </header>
