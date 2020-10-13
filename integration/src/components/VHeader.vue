f413cdbcc899d20551fbaef9500a2e5534709612
Emmanuel Durgoni <emmanuel@upian.com>
2020-10-13 12:32:32 +0200

    up

--- integration/src/components/VHeader.vue
+++ integration/src/components/VHeader.vue
@@ -3,13 +3,13 @@
     <div class="MainHeader__container max-w-4xl mx-auto px-4 py-6 bg-red-100 flex">
       <img src="@/assets/logo.svg" alt="Digits logo">
 
-      <nav class="ml-20 text-blue-dark">
-        <router-link to="/">Accueil</router-link>
-        <router-link to="/about">À propos</router-link>
+      <nav class=" ml-16">
+        <router-link class="link" to="/">Accueil</router-link>
+        <router-link class="link" to="/about">À propos</router-link>
       </nav>
 
       <div class="contact ml-auto ">
-        <a href="#">+00 123 456 789</a>
+        <a href="#" class="link">+00 123 456 789</a>
         <a href="#" class="bg-blue-button">Contact Us</a>
       </div>
     </div>
@@ -17,5 +17,12 @@
 </template>
 
 <style lang="postcss" scoped>
+.link {
+  @apply mr-10 p-3 text-blue-dark;
+}
 
+.link:focus,
+.link:hover {
+  @apply text-blue-button;
+}
 </style>
