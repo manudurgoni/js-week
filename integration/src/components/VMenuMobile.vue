043eeeac321c97a0f1c14ef6d73fc443f32ccc8b
Emmanuel Durgoni <emmanuel@upian.com>
2020-10-14 16:20:35 +0200

    up

--- integration/src/components/VMenuMobile.vue
+++ integration/src/components/VMenuMobile.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="MenuMobile">
-    <nav class="ml-16">
+  <div class="MenuMobile absolute w-full bg-pink-700">
+    <nav class="ml-16 flex flex-col justify-around items-center h-full">
       <router-link class="link" to="/">Accueil</router-link>
+      <a class="link"  href="#">Articles</a>
+      <a class="link"  href="#">Portfolio</a>
       <router-link class="link" to="/about">À propos</router-link>
     </nav>
   </div>
 </template>
+
+<style lang="postcss" scoped>
+.MenuMobile {
+  top: 72px;
+  height: calc(100vh - 72px);
+}
+</style>
