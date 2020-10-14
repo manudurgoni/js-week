d7d1d6e4473e5a55de51d35de7261fbcdacf93d7
Emmanuel Durgoni <emmanuel@upian.com>
2020-10-14 11:41:47 +0200

    update

--- integration/src/App.vue
+++ integration/src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <div id="app" class="open-sans">
+  <div id="app" class="open-sans" data-scroll-container>
     <v-header></v-header>
     <router-view/>
   </div>
 </template>
 
 <script>
+import LocomotiveScroll from 'locomotive-scroll'
 import VHeader from '@/components/VHeader'
 export default {
   components: {
     VHeader
+  },
+  mounted() {
+    this.scroll = new LocomotiveScroll({
+      el: this.$el.querySelector('[data-scroll-container]'),
+      smooth: true
+    });
   }
 }
 </script>
